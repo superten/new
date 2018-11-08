c7fc8713d7c5c991bd1dfba3c1841f5c52f60dd6
ryw <464139951@qq.com>
2018-11-08 18:54:07 +0800

    1

---
+++ new/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  new
+//
+//  Created by yw on 2018/11/8.
+//  Copyright © 2018 yw. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
