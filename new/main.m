2f4c0e7918817b02a009db3ce2330fc2980bfb50
ryw <464139951@qq.com>
2018-11-08 18:59:03 +0800

    2ci

--- new/main.m
+++ new/main.m
@@ -12,6 +12,10 @@ int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
         NSLog(@"Hello, World!");
+        
+        
+        
+        
     }
     return 0;
 }
